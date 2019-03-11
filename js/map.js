194d4d33fd7cf8f3065e5fa0c8ea699b08fb599a
Tuo Sun <tuosun@Tuos-MacBook-Pro.local>
2019-03-11 17:28:13 -0400

    live server & dictionary built

--- js/map.js
+++ js/map.js
@@ -1,12 +1,5 @@
-var data = [30, 86, 168, 281, 303, 365];
+// placeholder of histogram chart
 
-hist = d3.select(".chart")
-  .selectAll("div")
-  .data(data)
-    .enter()
-    .append("div")
-    .style("width", function(d) { return d + "px"; })
-    .text(function(d) { return d; });
 
 var width = 960,
     height = 700;
@@ -15,20 +8,17 @@ var width = 960,
     stroke = "rgba(0, 0, 0, 0.5)",
     strokeWidth = 0.1;
 
+
+// button of click to point office location
 var selectWorkLoc = false;
 document.getElementById('selectPt').onclick = function(){
   selectWorkLoc = true; console.log(selectWorkLoc);
   d3.selectAll('#workloc').remove();
 };
 
-console.log();
+// map generate from here
 
-// Define color scale
-var color = d3.scaleLinear()
-  .domain([1, 20])
-  .clamp(true)
-  .range(['#fff', '#409A99']);
-
+// define projection to NY
 var albersProjection = d3.geoAlbers()
     .scale( 100000 )
     .rotate( [73.977,0] )
@@ -39,39 +29,47 @@ var geoPath = d3.geoPath()
     .projection( albersProjection );
 
 
-
+// make a svg to hold the map
 var svg = d3.select("#mapcontainer").append("svg")
     .attr("width", width)
     .attr("height", height);
 
+// import data
 d3.queue()
     .defer(d3.json, "data/ny.json")
     .defer(d3.csv, "data/Rent_2017_5yr.csv")
     .await(ready);
 
-function findCenterCoord(l) {
-  return math.mean([math.max(l,0), math.min(l,0)],0);
-}
+// start construct an array of objects that includes geoid, centerCoord_long, centerCoord_lat, distance_to_center, median_rent
+var distanceRent = {}; 
 
 function ready(error, us, rent) {
   if (error) throw error;
 
-  // console.log(rent);
-  
+  // create a dictionary mapping GEOID: Median Gross Rent
+
+
+
   var rentByGeoid = {}; // Create empty object for holding dataset
   rent.forEach(function(d) {
-    // console.log(d['Geographic Identifier']);
-    // console.log(d['Geographic Identifier'].substr(7,));
-    // console.log(d);
-    rentByGeoid[parseFloat(d['Geographic Identifier'].substr(7,))] = + parseFloat(d['Median Gross Rent']);
+    var geoid = parseFloat(d['Geographic Identifier'].substr(7,));
+    rentByGeoid[geoid] = + parseFloat(d['Median Gross Rent']);
+    distanceRent[geoid] = {"geoid":geoid, "median_rent" : parseFloat(d['Median Gross Rent']), "centerCoord_long": 0 ,"centerCoord_lat": 0 ,"distance_to_center":-1};
   });
 
+  
+
+
+  // test loading
   console.log(rentByGeoid[360550116014]);
+
+  // Define color scale
 
   var color = d3.scaleThreshold()
     .domain([500, 1000, 1500, 2000, 2500])
     .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
 
+  // get features from map
   var features = topojson.feature(us, us.objects.cb_2017_36_bg_500k).features;
 
   var g = svg.append("g");
@@ -80,21 +78,36 @@ function ready(error, us, rent) {
     .selectAll("path")
       .data(features)
     .enter().append("path")
+      .attr("class", "blk")
       .attr("d", geoPath)
-      .filter(function(d) { return d.geometry != null && d.geometry.coordinates instanceof Array; })
-      .attr("centerCoord", function(d) {return math.mean(d.geometry.coordinates)})
+      .filter(function(d) { 
+        // console.log(d.geometry.coordinates.length);
+        // console.log(d.geometry.coordinates[0][0].length);
+        // && d.geometry.coordinates instanceof Array && d.geometry.coordinates.length[0] > 3 && d.geometry.coordinates[0][0].length == 2;
+        return d.geometry != null })
+      .attr("centerCoord_long", function(d) {
+        l = transpose(d.geometry.coordinates[0]); 
+        distanceRent[parseFloat(d.properties.GEOID)]["centerCoord_long"] = math.mean(l[0]);
+        return math.mean(l[0])
+      })
+      .attr("centerCoord_lat", function(d) {
+        l = transpose(d.geometry.coordinates[0]); 
+        distanceRent[parseFloat(d.properties.GEOID)]["centerCoord_lat"] = math.mean(l[1]);
+        return math.mean(l[1])
+      })
       .text(function(d) { return d; })
       .on('click', function(d) {
         if (selectWorkLoc==true) {
           // console.log('click');
           // console.log(features)
-          console.log(d.geometry.coordinates);
-          console.log(math.mean(d.geometry.coordinates,1));
+          // console.log(d.geometry.coordinates);
+          // console.log(math.mean(d.geometry.coordinates,1));
           // console.log(geoPath.centroid(d));
+
+          // get the center of the shape
           var t = geoPath.centroid(d)
           t = [Math.round(t[0]),Math.round(t[1])]
 
-          d.properties.centerCoord = 
           svg.selectAll(".centroid").data([t])
             .enter().append("circle")
               .attr("class", "centroid")
@@ -107,31 +120,57 @@ function ready(error, us, rent) {
               .attr("cy", function (t){ return t[1]; })
               .attr("transform", function(t) {return "translate("+t[0]+" px, "+t[1]+" px)";});
           selectWorkLoc=false;
+
+          var centroidCoord = [d3.select(this).attr("centerCoord_long"),d3.select(this).attr("centerCoord_lat")]
+
+          // calculate the distance between this point and others
+          // svg.selectAll(".blk").attr("distance", d3.select(this).attr("centerCoord_long"))
+          d3.selectAll(".blk").each(function(d,i){
+              var elt = d3.select(this);
+              // console.log( elt.attr("centerCoord_long") - centroidCoord[0]);
+              p1 = [elt.attr("centerCoord_long"),elt.attr("centerCoord_lat")];
+              p2 = centroidCoord;
+              distanceRent[parseFloat(d.properties.GEOID)]["distance_to_center"] = getDistance(p1,p2);
+              elt.attr("distance",getDistance(p1,p2));
+          });
+
+          console.log(distanceRent[360550116014]);
         }
       })
       .style("fill", function(d) {return color(rentByGeoid[parseFloat(d.properties.GEOID)])})
       .style("stroke", "black");
 
+  console.log(distanceRent[360550116014]);
+
+  // get all centroids
   var centroids = features.map(function (feature){
     return geoPath.centroid(feature);
   });
 
-  // var radians = d3.geo.distance([p1.Longitude, p1.Latitude], [p2.Longitude, p2.Latitude]);
-  // var numberMiles = radians * 3959; // radius of earth
-
-  // console.log(centroids);
-
-  // g.selectAll(".centroid").data(centroids)
-  //   .enter().append("circle")
-  //     .attr("class", "centroid")
-  //     .attr("fill", fill)
-  //     .attr("stroke", stroke)
-  //     .attr("stroke-width", strokeWidth)
-  //     .attr("r", radius)
-  //     .attr("cx", function (d){ return d[0]; })
-  //     .attr("cy", function (d){ return d[1]; });
 }
 
+var data = [30, 86, 168, 281, 303, 365];
 
+hist = d3.select("#histogram").select(".chart")
+  .selectAll("div")
+  .data(data)
+    .enter()
+    .append("div")
+    .style("width", function(d) { return d + "px"; })
+    .text(function(d) { return d; });
 
 d3.select(self.frameElement).style("height", height + "px");
+
+function findCenterCoord(l) {
+  return math.mean([math.max(l,0), math.min(l,0)],0);
+}
+function transpose(a)
+{
+  return a[0].map(function (_, c) { return a.map(function (r) { return r[c]; }); });
+}
+// calculate real distance between two points
+
+function getDistance(p1,p2){
+    var radians = d3.geoDistance(p1,p2);
+    return radians * 3959; // radius of earth
+}
